8d5508ef557845073157842a8689156498fb556c
SarthakSahoo1407 <sarthaksahoo.1520@gmail.com>
2024-04-22 23:31:56 +0530

    start push

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import laravel from 'laravel-vite-plugin';
+
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    laravel({
+        input: [
+            'resources/assets/sass/global.scss', 
+            'resources/assets/sass/default.scss'],
+        refresh: true,
+    }),
+    react()
+],
+
   build: { chunkSizeWarningLimit: 1600, }
 
 })
